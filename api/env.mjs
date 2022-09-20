810ef98c35c3cd90c0f7d0d293eff9cdc3ee0876
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-09-19 21:13:53 -0300

    Añadir URLMONGO a env.mjs

--- api/env.mjs
+++ api/env.mjs
@@ -2,14 +2,6 @@ import { config } from 'dotenv'
 
 config()
 
-const { DATABASE, HOST, PASSWORD, PORT_DB, PORT, USER } = process.env
+const { PORT, URLMONGO } = process.env
 
-const db = {
-  database: DATABASE,
-  host: HOST,
-  password: PASSWORD,
-  port: PORT_DB,
-  user: USER,
-}
-
-export { db, PORT, serverError }
+export { PORT, URLMONGO }
