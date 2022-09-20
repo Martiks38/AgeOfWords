09ab0cc936fd8e4aecb6f754a49734e3450f2107
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-09-20 14:38:20 -0300

    Crear middleware verifyAccountFields
    Válida los valores de la creación de una cuenta

---
+++ api/middleware/verifyAccountCreation.mjs
@@ -0,0 +1,38 @@
+const checkUserName = (userName) => {
+  if (typeof userName !== 'string') return false
+
+  let regexUserName = /^\w{5,16}$/i
+
+  return regexUserName.test(userName)
+}
+
+const checkPassword = (password) => {
+  if (typeof userName !== 'string') return false
+
+  let regexPassword = /^\S{8,20}$/
+
+  return regexPassword.test(password)
+}
+
+const checkEmail = (email) => {
+  if (typeof userName !== 'string') return false
+
+  if (userName.length < 7 || userName.length > 20) return false
+
+  let regexEmail = /^(\w+[/./-]?){1,}@[a-z]+[/.]\w{2,}$/
+
+  return regexEmail.test(email)
+}
+
+export const checkCreationFields = async (req, res, next) => {
+  const { userName, password, email } = req.body
+
+  let verifyUserName = checkUserName(userName)
+  let verifyPassword = checkPassword(password)
+  let verifyEmail = checkEmail(email)
+
+  if (verifyUserName && verifyPassword && verifyEmail)
+    res.status(400).json({ message: 'One or more fields are invalid' })
+
+  next()
+}
