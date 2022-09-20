fafa04428739ed9febe8d70d13600dcdf2a73e38
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-09-20 17:10:59 -0300

    Crear constante initialResults
    Almacena los valores iniciales de resultados al crear un usuario

--- api/const.mjs
+++ api/const.mjs
@@ -1,3 +1,13 @@
 const serverError = { status: 500, message: 'Internal Server Error' }
 
-export { serverError }
+const initialResults = {
+  try_1: 0,
+  try_2: 0,
+  try_3: 0,
+  try_4: 0,
+  try_5: 0,
+  try_6: 0,
+  total: 0,
+}
+
+export { initialResults, serverError }
