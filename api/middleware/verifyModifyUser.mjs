c3963a68f0bdd67a733b38e9b9c0a0eec6a147e1
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-09-21 15:27:29 -0300

    Añadir control de resultados

--- api/middleware/verifyModifyUser.mjs
+++ api/middleware/verifyModifyUser.mjs
@@ -1,8 +1,36 @@
 import { regex, serverError } from '../const.mjs'
+import userModel from '../Model/user.model.mjs'
 
 export const verifyModifyUser = async (req, res, next) => {
   try {
     const { field, newValue } = req.body
+
+    if (field === 'results') {
+      const keys = Object.keys(newValue)
+
+      for (let key of keys) {
+        if (!regex.test(key))
+          return res.status(400).json({ message: 'Invalid format' })
+      }
+
+      const values = Object.values(newValue)
+      const user = await userModel.findOne({ _id: req.userID })
+      const userValues = Object.values(user.results)
+
+      if (values.at(-1) - userValues.at(-1) !== 1) {
+        return res.status(400).json({ message: 'Inconsistency in the result' })
+      }
+
+      for (let ind = 0; ind < values.length - 1; ind++) {
+        let diff = values[ind] - userValues[ind]
+
+        if (diff < 0 || diff > 1) {
+          return res
+            .status(400)
+            .json({ message: 'Inconsistency in the result' })
+        }
+      }
+    }
 
     let regexFound = regex[field] ? true : false
 
