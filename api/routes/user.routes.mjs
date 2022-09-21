fe6a0cc0a378d37661245224807b05528edfb470
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-09-21 14:11:51 -0300

    Crear la ruta para modificar un usuario

--- api/routes/user.routes.mjs
+++ api/routes/user.routes.mjs
@@ -2,7 +2,9 @@ import { Router } from 'express'
 import userController from '../Controller/user.controller.mjs'
 
 import { accountExist } from '../middleware/accountExist.mjs'
+import { verifyToken } from '../middleware/authJwt.mjs'
 import { checkCreationFields } from '../middleware/verifyAccountCreation.mjs'
+import { verifyModifyUser } from '../middleware/verifyModifyUser.mjs'
 
 const router = Router()
 
@@ -14,6 +16,12 @@ router.post(
   '/user/create',
   [checkCreationFields, accountExist],
   userController.createUser
+)
+
+router.patch(
+  '/user/modify',
+  [verifyToken, verifyModifyUser],
+  userController.modifyUser
 )
 
 export default router
