d5c4652e4840e35b80902be6e21a44c7636e1e85
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-09-22 19:48:20 -0300

    Crear funcionalidad que verifique la validez de un campo del formulario

---
+++ app/src/utils/checkForm.ts
@@ -0,0 +1,48 @@
+import { regex } from '../const/regex'
+
+import { Dispatch, FocusEvent, SetStateAction } from 'react'
+import { SignUpForm } from '../interfaces'
+import { ErrorField } from '../types'
+
+const checkField = (
+  event: FocusEvent<HTMLInputElement>,
+  errorField: ErrorField,
+  setForm: Dispatch<SetStateAction<SignUpForm>>
+) => {
+  const { id, value } = event.currentTarget
+  let errorObj: ErrorField = {}
+
+  if (!regex[id].test(value)) {
+    errorObj = { ...errorField, [id]: true }
+
+    setForm({
+      message: 'One or more fields are invalid',
+      error: true,
+      errorField: errorObj,
+    })
+
+    event.currentTarget.classList.add('invalidField')
+  } else {
+    errorObj = { ...errorField, [id]: false }
+
+    let errorObjValues = Object.values(errorObj)
+
+    // Check if there are still invalid fields in the form.
+    // If this is the case, errors will continue to be reported.
+    errorObjValues.some((error) => error === true)
+      ? setForm({
+          message: 'One or more fields are invalid',
+          error: true,
+          errorField: errorObj,
+        })
+      : setForm({
+          message: '',
+          error: false,
+          errorField: errorObj,
+        })
+
+    event.currentTarget.classList.remove('invalidField')
+  }
+}
+
+export { checkField }
