c82f563de0f1b76a52c05a317cb87507516a9a9e
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-09-28 16:19:11 -0300

    Crear alias genérico de setState

--- app/src/types/index.ts
+++ app/src/types/index.ts
@@ -1,11 +1,20 @@
+import { Dispatch, SetStateAction } from 'react'
+
 type ErrorField = { [index: string]: boolean }
+
+type SetState<T> = Dispatch<SetStateAction<T>>
 
 type UserConnectedContextState = {
   isConnected: {
     username: string
     connected: boolean
   }
+  setIsConnected: SetState<{
+    username: string
+    connected: boolean
+  }>
+
   toggleConnected: (connected: boolean, username?: string) => void
 }
 
-export type { UserConnectedContextState, ErrorField }
+export type { ErrorField, UserConnectedContextState, SetState }
