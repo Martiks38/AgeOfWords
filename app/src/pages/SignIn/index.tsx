cd58f619eb51db8eb15c399b9654010c54da489e
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-10-02 19:54:16 -0300

    Borrar funcionalidades que no se implementarán

--- app/src/pages/SignIn/index.tsx
+++ app/src/pages/SignIn/index.tsx
@@ -57,12 +57,9 @@ function SignIn() {
               required
               onBlur={(e) => checkField(e, form.errorField, setForm)}
             />
-            <span className="form__label form__label_forgotPassword">
-              <label htmlFor="password">Password</label>
-              <Link href="">
-                <a>Forgot password?</a>
-              </Link>
-            </span>
+            <label className="form__label" htmlFor="password">
+              Password
+            </label>
             <input
               className="form__input"
               type="password"
