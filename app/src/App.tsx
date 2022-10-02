9961beac810aa5dee2f73947fa3d4c5c1071d87c
Giuliano Cappa <cappa.giuliano@gmail.com>
2022-10-02 18:03:24 -0300

    Añadir la ruta /game

--- app/src/App.tsx
+++ app/src/App.tsx
@@ -3,6 +3,7 @@ import { Route } from 'wouter'
 import UserProvider from './context/userConnect'
 
 import AccountSettings from './pages/AccountSettings'
+import Game from './pages/Game'
 import Home from './pages/Home'
 import SignIn from './pages/SignIn'
 import SignUp from './pages/SignUp'
@@ -11,6 +12,7 @@ function App() {
   return (
     <>
       <UserProvider>
+        <Route path="/game" component={Game} />
         <Route path="/login" component={SignIn} />
         <Route path="/signup" component={SignUp} />
         <Route path="/setting" component={AccountSettings} />
